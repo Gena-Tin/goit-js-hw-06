2db74723e8c5ec492cf3172954a77d6f8c103485
GenaTin <111846926+Gena-Tin@users.noreply.github.com>
2022-10-31 18:22:33 +0200

    task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,13 +1,40 @@
 // Напиши скрипт управления формой логина.
 
-// Обработка отправки формы form.login-form должна быть 
+// Обработка отправки формы form.login-form должна быть
 // по событию submit.
+
 // При отправке формы страница не должна перезагружаться.
-// Если в форме есть незаполненные поля, выводи alert с предупреждением 
+
+// Если в форме есть незаполненные поля, выводи alert с предупреждением
 // о том, что все поля должны быть заполнены.
-// Если пользователь заполнил все поля и отправил форму, 
-// собери значения полей в обьект, где имя поля будет именем 
-// свойства, а значение поля - значением свойства. 
+
+// Если пользователь заполнил все поля и отправил форму,
+// собери значения полей в обьект, где имя поля будет именем
+// свойства, а значение поля - значением свойства.
+
 // Для доступа к элементам формы используй свойство elements.
-// Выведи обьект с введенными данными в консоль и очисти значения 
+
+// Выведи обьект с введенными данными в консоль и очисти значения
 // полей формы методом reset.
+
+const loginForm = document.querySelector(".login-form");
+
+const formValidator = (event) => {
+    event.preventDefault();
+
+    const { elements } = event.currentTarget;
+
+    const emailFildValue = elements["email"].value;
+    const passwdFildValue = elements["password"].value;
+
+    if (emailFildValue === "" || passwdFildValue === "") {
+        alert("Some fields are empty!!!");
+        return;
+    }
+
+    console.log({ email: `${emailFildValue}`, password: `${passwdFildValue}` });
+
+    event.currentTarget.reset();
+};
+
+loginForm.addEventListener("submit", formValidator);
