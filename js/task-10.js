c3fbd8f21fcd3b0cc9f49b8ba7fcafafd9d7627c
GenaTin <111846926+Gena-Tin@users.noreply.github.com>
2022-11-22 11:34:06 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -28,6 +28,14 @@ const destroyBtn = document.querySelector("button[data-destroy]");
 const inputFild = document.querySelector("input");
 const divBox = document.querySelector("#boxes");
 
+const screenWidth = window.innerWidth;
+const screenHeight = window.innerHeight;
+
+document.addEventListener("keydown",(e) => {
+  if(e.key === "Enter"){createBoxes(inputFild.value);}
+  if(e.code === "Escape"){divBox.innerHTML = "";}
+})
+
 createBtn.addEventListener("click", () => {
   createBoxes(inputFild.value);
 });
@@ -44,13 +52,41 @@ function getRandomHexColor() {
 }
 
 function createBoxes(amount) {
+  
+  inputFild.value = "";
   for (let i = 1, size = 30; i <= amount; i++, size += 10) {
     const box = document.createElement("div");
+    const x = Math.round(Math.random()*screenWidth);
+    const y = Math.round(Math.random()*screenHeight);
+
+    // box.textContent = ` ${i} `;
+
     box.setAttribute("style",
       `
         display:inline-block;
+        position: absolute;
+        top:${y-size}px;
+        left: ${x-size}px;
         background-color: ${getRandomHexColor()};
+        border-radius: 5px;
         padding: ${size / 2}px;
+        font-size: ${size / 2}px;
+        color: #ffffff;
+        box-shadow: 5px 5px 8px 2px rgba(0, 0, 0, 0.3),
+         inset 5px 5px 8px 2px rgba(0, 0, 0, 0.3);
+
+      `
+    );
+    box.setAttribute("onmouseover",
+      `
+      this.style.backgroundColor ='${getRandomHexColor()}';
+      this.style.boxShadow = 'inset 5px 5px 8px 2px rgba(0, 0, 0, 0.3)';
+
+      `
+    );
+    box.setAttribute("onmouseout",
+      `
+      this.style.display='none';
       `
     );
     divBox.append(box);
